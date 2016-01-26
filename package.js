e441c04f4e14cf8b07323e50004652d29e69f8ae
Mike Cunneen <mike@cunneen.biz>
2016-01-26 23:15:22 +0800

    Oops - fix merge

--- package.js
+++ package.js
@@ -1,15 +1,8 @@
 Package.describe({
-<<<<<<< HEAD
   name: 'modweb:materialize-connection-status',
   summary: 'Materialized connection status.',
-  version: '0.0.3',
-  git: 'https://github.com/modweb/meteor-materialize-connection-status'
-=======
-  name: 'appworkshop:materialize-connection-status',
-  summary: 'Materialized connection status with cordova/phonegap support.',
   version: '0.0.4',
-  git: 'https://github.com/appworkshop/meteor-materialize-connection-status'
->>>>>>> bde3923... Workaround materialize issue where modal overlay remained (https://github.com/Dogfalo/materialize/issues/1647)
+  git: 'https://github.com/modweb/meteor-materialize-connection-status'
 });
 
 Package.onUse(function(api) {
